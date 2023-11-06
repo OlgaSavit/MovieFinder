989f6074ff04e5b0d0097e9c339c6eeb8a3aad3c
olgasavitska <savitskaoo9@ukr.net>
2023-11-06 22:19:55 +0200

    ..

--- src/api/requests/movie.js
+++
@@ -1,3 +0,0 @@
-import http from "../index";
-export const searchMovie = (params) => http.get(``, { params: params });
-export const getMovieDetails = (params) => http.get(``, { params: params });
--- src/pages/HomeView.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <SearchForm />
-</template>
-
-<script setup>
-import SearchForm from "@/components/SearchForm.vue";
-</script>
